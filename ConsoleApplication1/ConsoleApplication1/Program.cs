910f08abf2676ce25aea7ebfd8033b2929ab1369
Rustem <divey_R@mail.ru>
2015-11-02 17:05:31 +0300

    Created a new class "Point" and a "Draw" method for it!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,22 +10,20 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int x1 = 3;
-            int y1 = 4;
-            char symb1 = '$';
-            int x2 = 6;
-            int y2 = 8;
-            char symb2 = '#';
+            Point p1 = new Point();
+            p1.x = 3;
+            p1.y = 4;
+            p1.symb = '$';
+            p1.Draw();
 
-            Draw(x1, y1, symb1);
-            Draw(x2, y2, symb2);
+            Point p2 = new Point();
+            p2.x = 6;
+            p2.y = 8;
+            p2.symb = '#';
+            p2.Draw();
+
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char symb)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.WriteLine(symb);
-        }
     }
 }
