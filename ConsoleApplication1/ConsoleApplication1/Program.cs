ed80b249723263ea6fac65e69faf73d3fc5f67f5
Rustem <divey_R@mail.ru>
2015-11-02 16:32:15 +0300

    Added a "Draw" function, which allows to draw a symbol in a console

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,8 +10,22 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the jungle!");
+            int x1 = 3;
+            int y1 = 4;
+            char symb1 = '$';
+            int x2 = 6;
+            int y2 = 8;
+            char symb2 = '#';
+
+            Draw(x1, y1, symb1);
+            Draw(x2, y2, symb2);
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symb);
         }
     }
 }
